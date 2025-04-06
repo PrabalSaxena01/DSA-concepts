c2ae832e325af6e058a9eb95a1422033fae4fa85
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-06 17:58:49 +0530

    C++ Solution
          Submission Statistics:
          Question #: 151
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 42.40
          Memory: 23.784 MB
          Memory percentile: 12.11

--- 151. Reverse Words in a String/Solution.cpp
+++ 151. Reverse Words in a String/Solution.cpp
@@ -1,40 +1,17 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        string result = "";
-        string temp;
-        int n = s.size();
-        int j = n - 1;
-        int k = 0;
-        bool first = true;
-        for (int j = n - 1; j >= 0; j--) {
-            if (s[j] != ' ') {
-                temp += s[j];
-            } else if (temp != "") {
-                reverse(temp.begin(), temp.end());
-                if (first == true) {
-                    result = result + temp;
-                    first = false;
-                    
-                } else {
-                    result =  result+ " " + temp;
-                }
-                // if (j == 0) {
-                //     // reverse(temp.begin(), temp.end());
-                //     result = " " +result + temp;
-                //     temp = "";
-                // }
-                 temp = "";
-            }
-        }
-         if (!temp.empty()) {
-            reverse(temp.begin(), temp.end());
-            if (first) {
-                result += temp;
+          stringstream ss(s);
+        string word, result;
+
+        while (ss >> word) {               // Extracts words one by one
+            if (!result.empty()) {
+                result = word + " " + result;  // Add to the front of the result
             } else {
-                result += " " + temp;
+                result = word;                // First word, no space
             }
         }
+
         return result;
     }
 };
