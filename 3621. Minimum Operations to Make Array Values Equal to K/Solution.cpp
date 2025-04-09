90d2d8f3f507b2154631563cb0ec189559c5a198
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-09 09:31:03 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3621
          Language: C++
          Runtime: 22 ms
          Runtime percentile: 5.35
          Memory: 32.48 MB
          Memory percentile: 42.14

--- 3621. Minimum Operations to Make Array Values Equal to K/Solution.cpp
+++ 3621. Minimum Operations to Make Array Values Equal to K/Solution.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        set<int> s;
-        int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (k < nums[i]) {
-                s.insert(nums[i]);
-            } else if (k > nums[i]) {
+        unordered_set<int> s;
+        
+        for (int num:nums) {
+            if (k < num) {
+                s.insert(num);
+            } 
+             if (k > num) {
                 return -1;
             }
         }
