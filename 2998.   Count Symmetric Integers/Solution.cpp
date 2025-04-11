c24663c5aed29a5995146e41ff9e723373746b9e
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-11 09:31:18 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2998
          Language: C++
          Runtime: 69 ms
          Runtime percentile: 55.80
          Memory: 9.812 MB
          Memory percentile: 43.75

--- 2998.   Count Symmetric Integers/Solution.cpp
+++ 2998.   Count Symmetric Integers/Solution.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
-    bool check(int a) {
-        vector<int> s;
-        while (a > 0) {
-            s.push_back(a % 10);
-            a /= 10;
-        }
-        int n = s.size();
-         if (n % 2 != 0) return false;
-        int left = 0, right = 0;
-        for (int i = 0; i < (n / 2); i++) {
-            left += s[i];
-        }
-        for (int i = n - 1; i >= (n / 2); i--) {
-            right += s[i];
-        }
-        return left == right;
-    }
     int countSymmetricIntegers(int low, int high) {
-        int c = 0;
-        while (low <= high) {
-            if (check(low))
-                c++;
-            low++;
+          int count = 0;
+        for (int i = low; i <= high; i++) {
+            string s = to_string(i);
+            int n = s.length();
+            if (n % 2 != 0) continue; // Only even-digit numbers
+
+            int left = 0, right = 0;
+            for (int j = 0; j < n / 2; j++) {
+                left += s[j] - '0';
+                right += s[j + n / 2] - '0';
+            }
+
+            if (left == right) count++;
         }
-        return c;
+        return count;
     }
 };
