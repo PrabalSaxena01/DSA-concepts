ee130bd4f831a20527449f247fe4d9849a548207
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-12 12:34:08 +0530

    C++ Solution
          Submission Statistics:
          Question #: 219
          Language: C++
          Runtime: 73 ms
          Runtime percentile: 31.09
          Memory: 98.644 MB
          Memory percentile: 12.31

--- 219. Contains Duplicate II/Solution.cpp
+++ 219. Contains Duplicate II/Solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, pair<int, int>> mp;
+        unordered_map<int, int> mp;
 
         for (int i = 0; i < nums.size(); ++i) {
             int num = nums[i];
-               
-            if(mp.count(num) &&(i-mp[num].second)<=k)
-            return true;
-            mp[num].first++; 
-            mp[num].second = i; 
+            if (mp.count(num) && i - mp[num] <= k)
+                return true;
+            mp[num] = i;
         }
         return false;
     }
