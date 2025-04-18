beeed90bcc04babe6f167b8dbee97003fbbd5a56
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-18 11:27:24 +0530

    C++ Solution
          Submission Statistics:
          Question #: 38
          Language: C++
          Runtime: 5 ms
          Runtime percentile: 41.53
          Memory: 9.664 MB
          Memory percentile: 89.27

--- 38. Count and Say/Solution.cpp
+++ 38. Count and Say/Solution.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
     string countAndSay(int n) {
-
         string p = "1";
-     
-        for (int i = 1; i < n; i++) {
+
+        for (int i = 1; i < n; ++i) {
             string k = "";
-            int freq = 0;
-            char num = p[0];
-            for (int j = 0; j < p.length(); j++) {
-                if (p[j] == num) {
-                    freq++;
-                }
-                else if (p[j] != num) {
-                    k = k + to_string(freq) + num;
-                    num = p[j];
+            int freq = 1;
+            for (int j = 1; j < p.length(); ++j) {
+                if (p[j] == p[j - 1]) {
+                    ++freq;
+                } else {
+                    k += to_string(freq) + p[j - 1];
                     freq = 1;
-                }
-                if(j == p.length() - 1){
-                    num = p[j];
-                    k = k + to_string(freq) + num;
                 }
             }
 
+            k += to_string(freq) + p.back();
             p = k;
         }
+
         return p;
     }
 };
