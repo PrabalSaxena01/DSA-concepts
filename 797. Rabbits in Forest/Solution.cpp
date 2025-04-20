9725e6e6df402921a2ebef6a56092397f7f8cde8
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-20 12:18:32 +0530

    C++ Solution
          Submission Statistics:
          Question #: 797
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 12.18 MB
          Memory percentile: 71.00

--- 797. Rabbits in Forest/Solution.cpp
+++ 797. Rabbits in Forest/Solution.cpp
@@ -2,12 +2,13 @@ class Solution {
 public:
     int numRabbits(vector<int>& answers) {
         int res = 0;
-        unordered_map<int, float> freq;
+        unordered_map<int, int> freq;
         for (int n : answers) {
             freq[n]++;
         }
         for (const auto& [num, count] : freq) {
-            res += (ceil(count / (num + 1))) * (num + 1);
+           int req = (count / (num + 1)) + (count%(num+1)==0?0:1);
+            res += req * (num + 1);
         }
         return res;
     }
