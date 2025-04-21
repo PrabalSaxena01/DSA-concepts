57785ca27283fa92651d9064159ea960dabb0e9a
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-21 20:48:11 +0530

    C++ Solution
          Submission Statistics:
          Question #: 2249
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 112.492 MB
          Memory percentile: 97.58

--- 2249. Count the Hidden Sequences/Solution.cpp
+++ 2249. Count the Hidden Sequences/Solution.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
-        long long min = INT_MAX;
-        long long max = INT_MIN;
-        long long x = 0;
-        vector<int> arr;
-        arr.push_back(x);
-        for (long long num : differences) {
-
-            x += num;
-            arr.push_back(x);
+        long long minPrefix = 0, maxPrefix = 0;
+        long long prefix = 0;
+        
+        for (int diff : differences) {
+            prefix += diff;
+            minPrefix = min(minPrefix, prefix);
+            maxPrefix = max(maxPrefix, prefix);
         }
-        for (int num : arr) {
-            if (num < min) {
-                min = num;
-            }
-            if (num > max) {
-                max = num;
-            }
-        }
-        long long res = (upper - lower + 1) - (max - min);
-        res = res > 0 ? res : 0;
-        return (int)res;
+        
+        long long res = (upper - lower + 1) - (maxPrefix - minPrefix);
+        return max(0, (int)res);
     }
 };
