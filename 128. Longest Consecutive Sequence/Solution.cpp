4444242b04bf5b347ce32f556a4f8d9fcf258954
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-22 23:41:11 +0530

    C++ Solution
          Submission Statistics:
          Question #: 128
          Language: C++
          Runtime: 77 ms
          Runtime percentile: 74.16
          Memory: 88.94 MB
          Memory percentile: 26.90

--- 128. Longest Consecutive Sequence/Solution.cpp
+++ 128. Longest Consecutive Sequence/Solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        if(nums.size() < 2) return nums.size();
         int maxcount = 0;
-        unordered_set<int> s;
-        for (int num : nums) {
-            s.insert(num);
-        }
-        for(auto num:s){
-            if(s.find(num-1)!=s.end()) continue;
+        unordered_set<int> s(nums.begin(), nums.end());
+
+        for (auto num : s) {
+            if (s.find(num - 1) != s.end())
+                continue;
             int ser = 1;
-            while(s.find(++num)!=s.end()) ser++;
-            maxcount = max(maxcount,ser);
+            while (s.find(++num) != s.end())
+                ser++;
+            maxcount = max(maxcount, ser);
         }
-    return maxcount;
+        return maxcount;
     }
 };
