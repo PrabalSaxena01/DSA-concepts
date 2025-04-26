131fed544712fa0b7b2009df3be8fd991f3f1951
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-04-26 22:02:13 +0530

    C++ Solution
          Submission Statistics:
          Question #: 3447
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 8.512 MB
          Memory percentile: 48.08

---
+++ 3447. Clear Digits/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int charIndex = 0;
+        while (charIndex < s.size()) {
+            if (isdigit(s[charIndex])) {
+                s.erase(charIndex, 1);
+                s.erase(charIndex - 1, 1);
+                charIndex--;
+            } else {
+                charIndex++;
+            }
+        }
+        return s;
+    }
+};
