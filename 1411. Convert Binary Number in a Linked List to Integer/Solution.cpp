89fd7c41bb301268c8044e6da68d053f4018bd28
Prabal <160031176+PrabalSaxena01@users.noreply.github.com>
2025-07-14 18:49:58 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1411
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 10.672 MB
          Memory percentile: 39.86

--- 1411. Convert Binary Number in a Linked List to Integer/Solution.cpp
+++ 1411. Convert Binary Number in a Linked List to Integer/Solution.cpp
@@ -11,16 +11,11 @@
 class Solution {
 public:
     int getDecimalValue(ListNode* head) {
-        vector<int> result;
-        auto first = head;
-        while (first != NULL) {
-            result.insert(result.begin(), first->val);
-            first = first->next;
+          int num = 0;
+        while (head != nullptr) {
+            num = (num << 1) + head->val;
+            head = head->next;
         }
-        int ans = 0;
-        for (int i = 0; i < result.size(); i++) {
-            ans  += result[i]*pow(2,i);
-        }
-        return ans;
+        return num;
     }
 };
